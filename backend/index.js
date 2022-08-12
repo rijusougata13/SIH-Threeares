be26d35947dfd2fcc652d3b5c4f667760341d9a0
Anik Das <cse2019023@rcciit.org.in>
2022-08-12 23:45:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,37 @@
 const { default: axios } = require('axios');
-const bodyParser=require('body-parser');
-const cors=require('cors');
-const express=require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
 
 
-const app=express();
+const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    cors()
-  );
+  cors()
+);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-    }
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}
 );
 
 app.get("/distance", async (req, res) => {
-   const result= await axios.get(`https://www.distance24.org/route.json?stops=${req.query.origin}|${req.query.destination}`);
-   console.log(result);
-   return res.json(result.data.distance);
-} );
+  const result = await axios.get(`https://distanceto.herokuapp.com/distance?pin1=${req.query.origin}&pin2=${req.query.destination}`);
+  console.log(result.data);
+  return res.json(result.data);
+});
 
-app.get("/",(req,res)=>{
-    return res.status(200).json({
-        message:"Welcome to the backend"
-    })
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Welcome to the backend"
+  })
 })
 
-const port=5000;
+const port = 5000;
 app.listen(process.env.PORT || port, () => {
   console.log(`listening on 5000`);
 });
