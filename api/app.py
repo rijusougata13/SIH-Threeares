77326c71e1e115a816926aff3d6530807e31a607
Anik Das <cse2019023@rcciit.org.in>
2022-08-26 13:13:47 +0530

    api update

--- api/app.py
+++ api/app.py
@@ -1,17 +1,26 @@
 
 from flask import Flask, jsonify, request
-import pgeocode
+from joblib import load
 
 app = Flask(__name__)
-dist = pgeocode.GeoDistance('in')
+model = load('model.joblib')
 
 
-@app.route('/distance', methods=['GET'])
-def distance():
-    pin1 = request.args.get('pin1')
-    pin2 = request.args.get('pin2')
-    res = dist.query_postal_code(pin1, pin2)
-    return jsonify(res)
+@app.route('/emission', methods=['GET'])
+def emission():
+    enginesize = request.args.get('esize')
+    print(type(enginesize))
+    cylinders = request.args.get('cyd')
+    fuelType = request.args.get('type')
+    fuelConsumption = request.args.get('cons')
+    F1, F2, F3 = (fuelType.split(','))
+    print(F2)
+    input = [float(enginesize), float(cylinders),
+             float(fuelConsumption), float(F1), float(F2), float(F3)]
+
+    res = model.predict([input]).flatten()
+    # print(res.flatten())
+    return (jsonify(res[0]))
 
 
 # driver function
